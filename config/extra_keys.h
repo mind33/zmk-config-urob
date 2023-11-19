a7b98425b1c09b5a5c13b3906bc106182aabf4ec
mind33 <mind33@users.noreply.github.com>
2023-11-19 11:36:52 +0400

    Update extra_keys.h

--- config/extra_keys.h
+++ config/extra_keys.h
@@ -1,6 +1,20 @@
 // The default layout has 34 keys. Additional keys can be added by pre-setting any of
 // the macros defined in this file to one or more keys before sourcing this file.
 
+/* between left and right half */
+#if !defined X_MT  // top row, middle
+    #define X_MT
+#endif
+#if !defined X_MM  // middle row, middle
+    #define X_MM
+#endif
+#if !defined X_MB  // bottom row, middle
+    #define X_MB
+#endif
+#if !defined X_MH  // thumb row, middle
+    #define X_MH
+#endif
+
 /* left of left half */
 #if !defined X_LT  // top row, left
     #define X_LT
@@ -15,19 +29,7 @@
     #define X_LH
 #endif
 
-/* between left and right half */
-#if !defined X_MT  // top row, middle
-    #define X_MT
-#endif
-#if !defined X_MM  // middle row, middle
-    #define X_MM
-#endif
-#if !defined X_MB  // bottom row, middle
-    #define X_MB
-#endif
-#if !defined X_MH  // thumb row, middle
-    #define X_MH
-#endif
+
 
 /* right of right half */
 #if !defined X_RT  // top row, right
