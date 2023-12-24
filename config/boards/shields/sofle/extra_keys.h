095fb51ec5ec992b8f7a28defe3a47233a85773d
mind33 <mind33@users.noreply.github.com>
2023-12-24 22:18:51 +0400

    asdsa

---
+++ config/boards/shields/sofle/extra_keys.h
@@ -0,0 +1,196 @@
+// The default layout has 34 keys. Additional keys can be added by pre-setting any of
+// the macros defined in this file to one or more keys before sourcing this file.
+
+/* between left and right half */
+#if !defined X_MT  // top row, middle
+    #define X_MT
+#endif
+#if !defined X_MM  // middle row, middle
+    #define X_MM
+#endif
+#if !defined X_MB  // bottom row, middle
+    #define X_MB
+#endif
+#if !defined X_MH  // thumb row, middle
+    #define X_MH
+#endif
+
+/* left of left half */
+#if !defined X_LT0  // top row, left
+    #define X_LT0
+#endif
+#if !defined X_LM0  // middle row, left
+    #define X_LM0
+#endif
+#if !defined X_LB0  // bottom row, left
+    #define X_LB0
+#endif
+#if !defined X_LH0  // thumb row, left
+    #define X_LH0
+#endif
+
+
+
+/* right of right half */
+#if !defined X_RT0  // top row, right
+    #define X_RT0
+#endif
+#if !defined X_RM0  // middle row, right
+    #define X_RM0
+#endif
+#if !defined X_RB0  // bottom row, right
+    #define X_RB0
+#endif
+#if !defined X_RH0  // thumb row, right
+    #define X_RH0
+#endif
+
+/* left of left half */
+#if !defined X_LT1  // top row, left
+    #define X_LT1
+#endif
+#if !defined X_LM1  // middle row, left
+    #define X_LM1
+#endif
+#if !defined X_LB1  // bottom row, left
+    #define X_LB1
+#endif
+#if !defined X_LH1  // thumb row, left
+    #define X_LH1
+#endif
+
+
+
+/* right of right half */
+#if !defined X_RT1  // top row, right
+    #define X_RT1
+#endif
+#if !defined X_RM1  // middle row, right
+    #define X_RM1
+#endif
+#if !defined X_RB1  // bottom row, right
+    #define X_RB1
+#endif
+#if !defined X_RH1  // thumb row, right
+    #define X_RH1
+#endif
+
+/* left of left half */
+#if !defined X_LT2  // top row, left
+    #define X_LT2
+#endif
+#if !defined X_LM2  // middle row, left
+    #define X_LM2
+#endif
+#if !defined X_LB2  // bottom row, left
+    #define X_LB2
+#endif
+#if !defined X_LH2  // thumb row, left
+    #define X_LH2
+#endif
+
+
+
+/* right of right half */
+#if !defined X_RT2  // top row, right
+    #define X_RT2
+#endif
+#if !defined X_RM2  // middle row, right
+    #define X_RM2
+#endif
+#if !defined X_RB2  // bottom row, right
+    #define X_RB2
+#endif
+#if !defined X_RH2  // thumb row, right
+    #define X_RH2
+#endif
+
+/* left of left half */
+#if !defined X_LT3  // top row, left
+    #define X_LT3
+#endif
+#if !defined X_LM3  // middle row, left
+    #define X_LM3
+#endif
+#if !defined X_LB3  // bottom row, left
+    #define X_LB3
+#endif
+#if !defined X_LH3  // thumb row, left
+    #define X_LH3
+#endif
+
+
+
+/* right of right half */
+#if !defined X_RT3  // top row, right
+    #define X_RT3
+#endif
+#if !defined X_RM3  // middle row, right
+    #define X_RM3
+#endif
+#if !defined X_RB3  // bottom row, right
+    #define X_RB3
+#endif
+#if !defined X_RH3  // thumb row, right
+    #define X_RH3
+#endif
+
+/* left of left half */
+#if !defined X_LT4  // top row, left
+    #define X_LT4
+#endif
+#if !defined X_LM4  // middle row, left
+    #define X_LM4
+#endif
+#if !defined X_LB4  // bottom row, left
+    #define X_LB4
+#endif
+#if !defined X_LH4  // thumb row, left
+    #define X_LH4
+#endif
+
+
+
+/* right of right half */
+#if !defined X_RT4  // top row, right
+    #define X_RT4
+#endif
+#if !defined X_RM4  // middle row, right
+    #define X_RM4
+#endif
+#if !defined X_RB4  // bottom row, right
+    #define X_RB4
+#endif
+#if !defined X_RH4  // thumb row, right
+    #define X_RH4
+#endif
+
+/* left of left half */
+#if !defined X_LT5  // top row, left
+    #define X_LT5
+#endif
+#if !defined X_LM5  // middle row, left
+    #define X_LM5
+#endif
+#if !defined X_LB5  // bottom row, left
+    #define X_LB5
+#endif
+#if !defined X_LH5  // thumb row, left
+    #define X_LH5
+#endif
+
+
+
+/* right of right half */
+#if !defined X_RT5  // top row, right
+    #define X_RT5
+#endif
+#if !defined X_RM5  // middle row, right
+    #define X_RM5
+#endif
+#if !defined X_RB5  // bottom row, right
+    #define X_RB5
+#endif
+#if !defined X_RH5  // thumb row, right
+    #define X_RH5
+#endif
