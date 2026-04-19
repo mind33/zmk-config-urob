1024585c70d01d2c03930f44ea7c41cb80ed46f9
mind33 <mind33@users.noreply.github.com>
2026-04-19 18:17:39 +0100

    Add key definitions for base.keymap

---
+++ config/extra_keys.h
@@ -0,0 +1,19 @@
+#pragma once
+
+/* Left hand keys */
+#define X_LT &none
+#define X_LM &none
+#define X_LB &none
+#define X_LH &none
+
+/* Middle keys */
+#define X_MT &none
+#define X_MM &none
+#define X_MB &none
+#define X_MH &none
+
+/* Right hand keys */
+#define X_RT &none
+#define X_RM &none
+#define X_RB &none
+#define X_RH &none
